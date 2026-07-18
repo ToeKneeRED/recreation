71d1c797f003258541f7f70a030339a81b5a49ae
Vincent Hengel <prelink835@gmail.com>
2026-07-18 03:24:58 +0200

    terrain: define NOMINMAX before <windows.h> so numeric_limits::max() compiles on MSVC

--- engine/world/terrain_edits.cc
+++ engine/world/terrain_edits.cc
@@ -13,6 +13,9 @@
 
 #if defined(_WIN32)
 #define WIN32_LEAN_AND_MEAN
+// Keep <windows.h> from defining min/max as macros; they clobber
+// std::numeric_limits<>::max() / std::max() used throughout this file.
+#define NOMINMAX
 #include <windows.h>
 #endif
 
